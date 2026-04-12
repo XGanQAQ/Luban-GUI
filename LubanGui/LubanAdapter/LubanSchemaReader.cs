876432228febab0876f4b94e2a30bfbbe6816c65
GanX <2423855310@qq.com>
2026-04-12 09:04:01 +0800

    fix(): 左侧表格列表可以显示表格了

--- LubanGui/LubanAdapter/LubanSchemaReader.cs
+++ LubanGui/LubanAdapter/LubanSchemaReader.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Luban;
 using Luban.RawDefs;
@@ -25,6 +26,7 @@ public class LubanSchemaReader : ILubanSchemaReader
     public LubanSchemaReader(ILogger<LubanSchemaReader> logger)
     {
         _logger = logger;
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
     }
 
     public async Task<IReadOnlyList<TableSchemaDto>> ReadTablesAsync(string confPath)
